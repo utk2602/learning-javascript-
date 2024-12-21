130ba523171957cf79ca6fa092c2191c839fcf7b
utk2602 <166751207+utk2602@users.noreply.github.com>
2024-12-21 11:46:06 +0530

    21-12-2024

---
+++ app.js
@@ -0,0 +1,55 @@
+/*var life = 100; //this is how you comment 
+var name ="Utkarsh";
+var checkout =false;
+var box;//console's output will show undefined 
+
+
+
+const life=100;
+life=life-60//wont change the output since lfe, we cannot re
+console.log(life);*/
+
+const name = "Utkarsh";
+
+/*function logger(){
+    console.log('Party time');
+    console.log('Party time');
+    console.log('Party time');
+    console.log('Party time');
+    console.log('Party time');
+}*/
+
+/*logger()*/;//for running the function 
+
+// all the standard function properties work over here too 
+
+
+/*function adder(num1,num2){
+    console.log(num1+num2);
+}
+
+adder(5,10);
+
+// .toUpperCase converts the whole string to upper case
+
+
+const toUpper = () =>{
+    asaadddwa;
+}*/
+
+
+
+//String concaetation 
+
+
+console.log("hello my name is "+"utk");
+
+
+console.log(`hello my name is ${name}`);
+
+
+//if we concatenate a string and a number it will take both the values as string only
+
+
+can be verified using 
+console.log(type of xyz);
